fff8b2bbc07c9192c8af60050b83088d94288175
Jessy <jessyshi1997@gmail.com>
2025-11-26 15:40:01 +0800

    employees table

---
+++ src/main/java/employeesdb/Employees.java
@@ -0,0 +1,55 @@
+package employeesdb;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="employees")
+public class Employees {
+    @Id
+    @Column(name ="emp_no")
+    int empNo;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name ="first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "gender")
+    private char gender;
+
+    @Column(name= "hire_date")
+    private LocalDate hireDate;
+
+    public int getEmpNo() {return empNo; }
+
+    public void setEmpNo(int empNo) {this.empNo = empNo;}
+
+    public LocalDate getBirthDate() {return birthDate;}
+
+    public void setBirthDate(LocalDate birthDate) {this.birthDate = birthDate;}
+
+    public String getFirstName() {return firstName;}
+
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+
+    public String getLastName() {return lastName;}
+
+    public void setLastName(String lastName) {this.lastName = lastName;}
+
+    public char getGender() {return gender;}
+
+    public void setGender(char gender) {this.gender = gender;}
+
+    public LocalDate getHireDate() {return hireDate;}
+
+    public void setHireDate(LocalDate hireDate) {this.hireDate = hireDate;}
+}
