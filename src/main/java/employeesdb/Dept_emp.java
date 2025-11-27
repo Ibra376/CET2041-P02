b0207dee4d9971616147419513100aff60b85085
bkhou <m.ibrahim.376f@gmail.com>
2025-11-27 16:01:09 +0800

    added relationships and edited toString in Dept_emp.java and Dept_manager.java - Ibrahim

--- src/main/java/employeesdb/Dept_emp.java
+++ src/main/java/employeesdb/Dept_emp.java
@@ -9,8 +9,6 @@ import java.util.Objects;
 @Table(name = "dept_emp")
 public class Dept_emp {
 
-    public Dept_emp() {}
-
     @EmbeddedId
     private DeptEmpId id;
 
@@ -19,6 +17,22 @@ public class Dept_emp {
 
     @Column(name = "to_date")
     private LocalDate toDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "emp_no", insertable = false, updatable = false)
+    private Employees employee;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dept_no", insertable = false, updatable = false)
+    private Departments department;
+
+    public Dept_emp() {}
+
+    public Dept_emp(DeptEmpId id, LocalDate fromDate, LocalDate toDate) {
+        this.id = id;
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+    }
 
     public DeptEmpId getId() { return id; }
     public void setId(DeptEmpId id) { this.id = id; }
@@ -29,16 +43,27 @@ public class Dept_emp {
     public LocalDate getToDate() { return toDate; }
     public void setToDate(LocalDate toDate) { this.toDate = toDate; }
 
+    @Override
+    public String toString() {
+        return "From: "+fromDate+", To: "+toDate+", EmpNo: "+id.getEmpNo()+"," +
+                " DeptNo: "+id.getDeptNo();
+    }
+
     @Embeddable
     public static class DeptEmpId implements Serializable {
-
-        public DeptEmpId() {}
 
         @Column(name = "emp_no")
         private int empNo;
 
         @Column(name = "dept_no")
         private String deptNo;
+
+        public DeptEmpId() {}
+
+        public DeptEmpId(int empNo, String deptNo) {
+            this.empNo = empNo;
+            this.deptNo = deptNo;
+        }
 
         public int getEmpNo() { return empNo; }
         public void setEmpNo(int empNo) { this.empNo = empNo; }
--- src/main/java/employeesdb/Dept_manager.java
+++ src/main/java/employeesdb/Dept_manager.java
@@ -9,8 +9,6 @@ import java.util.Objects;
 @Table(name = "dept_manager")
 public class Dept_manager {
 
-    public Dept_manager() {}
-
     @EmbeddedId
     private DeptManagerId id;
 
@@ -19,6 +17,22 @@ public class Dept_manager {
 
     @Column(name = "to_date")
     private LocalDate toDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "emp_no", insertable = false, updatable = false)
+    private Employees employee;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dept_no", insertable = false, updatable = false)
+    private Departments department;
+
+    public Dept_manager() {}
+
+    public Dept_manager(DeptManagerId id, LocalDate fromDate, LocalDate toDate) {
+        this.id = id;
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+    }
 
     public DeptManagerId getId() { return id; }
     public void setId(DeptManagerId id) { this.id = id; }
@@ -29,16 +43,26 @@ public class Dept_manager {
     public LocalDate getToDate() { return toDate; }
     public void setToDate(LocalDate toDate) { this.toDate = toDate; }
 
+    public String toString() {
+        return "From: "+fromDate+", To: "+toDate+", EmpNo: "+id.getEmpNo()+"," +
+                " DeptNo: "+id.getDeptNo();
+    }
+
     @Embeddable
     public static class DeptManagerId implements Serializable {
-
-        public DeptManagerId() {}
 
         @Column(name = "dept_no")
         private String deptNo;
 
         @Column(name = "emp_no")
         private int empNo;
+
+        public DeptManagerId() {}
+
+        public DeptManagerId(int empNo, String deptNo) {
+            this.empNo = empNo;
+            this.deptNo = deptNo;
+        }
 
         public String getDeptNo() { return deptNo; }
         public void setDeptNo(String deptNo) { this.deptNo = deptNo; }
