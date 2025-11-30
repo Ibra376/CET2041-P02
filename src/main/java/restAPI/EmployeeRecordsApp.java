20eda48f88a2860791549df367a01c95dc41fee9
Jessy <jessyshi1997@gmail.com>
2025-11-30 12:07:49 +0800

    commented out DepartmentRecords
    to work on single REST API file going forward: EmployeeRecords.java

--- src/main/java/restAPI/EmployeeRecordsApp.java
+++ src/main/java/restAPI/EmployeeRecordsApp.java
@@ -14,15 +14,15 @@ public class EmployeeRecordsApp extends Application {
     public Set<Class<?>> getClasses() {
         Set<Class<?>> s = new HashSet<Class<?>>();
         s.add(EmployeeRecords.class);
-        s.add(DepartmentRecords.class);
+//        s.add(DepartmentRecords.class);
         return s;
     }
 
-    @Override
-    public Map<String, Object> getProperties() {
-        Map<String, Object> properties = new HashMap<>();
-        properties.put("jersey.config.server.provider.classnames",
-                "org.glassfish.jersey.media.multipart.MultiPartFeature");
-        return properties;
-    }
+//    @Override
+//    public Map<String, Object> getProperties() {
+//        Map<String, Object> properties = new HashMap<>();
+//        properties.put("jersey.config.server.provider.classnames",
+//                "org.glassfish.jersey.media.multipart.MultiPartFeature");
+//        return properties;
+//    }
 }
